ca2f3e0600500af1c153ae0b1154a743c23e15df
Waiwitz <waiwit.laoniyomthai@gmail.com>
2025-05-25 23:09:23 +0700

    fix: comment out base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/play_assignment/'
+  // base: '/play_assignment/'
 })
